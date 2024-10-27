c36d1c9a77a48eccc6be9c5148aa9dbd421320f9
Ameer <tmsyameer@gmail.com>
2024-10-27 10:48:31 +0530

    fixed not changing icon to moon when the system defaul mode is dark

--- src/myComponents/icon/ThemeColor.jsx
+++ src/myComponents/icon/ThemeColor.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
@@ -12,10 +12,16 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 function ThemeColor() {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, resolvedTheme } = useTheme();
+  const [currentTheme, setCurrentTheme] = React.useState(resolvedTheme);
+
+  // Sync current theme with resolvedTheme whenever it changes
+  React.useEffect(() => {
+    setCurrentTheme(theme === "system" ? resolvedTheme : theme);
+  }, [theme, resolvedTheme]);
 
   // Determine button styles based on the current theme
-  const buttonStyles = theme === 'dark' ? 'bg-black text-white' : 'bg-gray-800 text-white';
+  const buttonStyles = currentTheme === "dark" ? "bg-black !text-white" : "!bg-white text-black";
 
   return (
     <DropdownMenu>
@@ -23,26 +29,20 @@ function ThemeColor() {
         <Button
           variant="outline"
           size="icon"
-          className={`${buttonStyles} border transition-colors duration-300 rounded-full ${theme !== 'dark' && '!bg-white'}`} // Smooth transition effect
+          className={`${buttonStyles} border transition-colors duration-300 rounded-full focus:border-none`}
         >
-          {theme === 'dark' ? (
-            <Moon className="h-[1.2rem] w-[1.2rem] transition-all" />
-          ) : (
+          {currentTheme !== "dark" ? (
             <Sun className="h-[1.2rem] w-[1.2rem] transition-all text-black" />
+          ) : (
+            <Moon className="h-[1.2rem] w-[1.2rem] transition-all text-white" />
           )}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
